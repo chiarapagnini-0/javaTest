3063db6af67e296e1570edc05c2dbbe825b54c80
kiara <kiara@LAPTOP-2TNSCJ2J.homenet.telecomitalia.it>
2024-09-27 15:51:50 +0200

    metodo2

--- src/main/java/it/generationitaly/app/controller/App.java
+++ src/main/java/it/generationitaly/app/controller/App.java
@@ -5,9 +5,15 @@ public class App {
 	public static void main(String[] args) {
        System.out.println("Hello World!");
        metodo1();
+       metodo2();
 	}
 
 	private static void metodo1() {
 		System.out.println("metodo1");
+	}
+	
+	private static void metodo2() {
+		System.out.println("metodo2");
+		
 	}
 }
